83153a98183f0179b8bd5b183f503fa4f6a687ee
knightrun <to_promise@nate.com>
2021-03-09 23:14:52 +0900

    이미지 추가

--- utils/decorators.ts
+++ utils/decorators.ts
@@ -1,7 +1,7 @@
 import { createDecorator } from 'vue-class-component'
 
 export const Functional = (options: boolean) => {
-    return createDecorator((componentOptions, key) => {
+    return createDecorator((componentOptions: any , key: string) => {
         if (!Array.isArray(componentOptions[key])) {
             componentOptions[key] = options
         }
