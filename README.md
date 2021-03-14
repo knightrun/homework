8edc4503c762a4fe7d5cedabc37dc4f485a4b2f6
knightrun <to_promise@nate.com>
2021-03-14 10:27:17 +0900

    homework_1 작업중

--- README.md
+++ README.md
@@ -9,4 +9,7 @@ $ yarn dev
 
 homewark_1 : http://localhost:3000/html/homework.html
 homewark_2 : http://localhost:3000/join/form
+
+
+* 해상도 375*667px 기준으로 작업하였습니다. 
 ```
--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,45 +1,72 @@
-let viewportSize, viewportOffset;
+(function(window, document, undefined){
+  'use strict';
 
-function getViewportSize() {
-  return {
-    width: window.innerWidth,
-    height: window.innerHeight
-  };
-}
+  function getViewportSize() {
+    return {
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
 
-function getViewportOffset() {
-  return {
-    top:  window.pageYOffset || document.documentElement.scrollTop   || document.body.scrollTop,
-    left: window.pageXOffset || document.documentElement.scrollLeft  || document.body.scrollLeft
-  };
-}
+  function getViewportOffset() {
+    return {
+      top:  window.pageYOffset || document.documentElement.scrollTop   || document.body.scrollTop || 0,
+      left: window.pageXOffset || document.documentElement.scrollLeft  || document.body.scrollLeft || 0
+    };
+  }
 
-function addEvent() {
-  const btnPopup = document.querySelector('.btn_popup');
-  const popupWrap = document.querySelector('.popup_wrap');
-  const btnClose = document.querySelector('.btn_close');
+  function init() {
+    const btnPopup = document.querySelector('.btn_popup');
+    const popupWrap = document.querySelector('.popup_wrap');
+    const btnClose = document.querySelector('.btn_close');
 
-  btnPopup.addEventListener('click', function(){
-    popupWrap.classList.add('bounce')
-  });
+    btnPopup.addEventListener('click', function(){
+      popupWrap.classList.add('bounce');
+    });
 
-  btnClose.addEventListener('click', function(){
-    popupWrap.classList.remove('bounce')
-  });
+    btnClose.addEventListener('click', function(){
+      popupWrap.classList.remove('bounce');
+    });
+
+    window.addEventListener('scroll', function() {
+      const target = document.querySelectorAll('.scrollable');
+      let i = 0,
+          len = target.length,
+          scrolled = getViewportOffset().top;
+
+      for(i; i < len; i++){
+        let dataSet = Object.assign({}, target[i].dataset),
+            keys = Object.keys(dataSet),
+            posY = 0;
+
+        keys.forEach(function(val, index){
+          if(scrolled > +val){
+            if(index < keys.length - 1){
+              let firstKey = +keys[index];
+              let secondKey = +keys[index + 1];
+
+              if(scrolled >= firstKey && scrolled <= secondKey){
+                let firstVal = +dataSet[keys[index]],
+                    secondVal = +dataSet[keys[index + 1]],
+                    distance = Math.abs(secondVal - firstVal),
+                    ratio = distance/(secondKey - firstKey),
+                    direction = secondVal > 0 ? 1 : -1;
+
+                posY = +(firstVal + (direction * (scrolled - firstKey) * ratio)).toFixed(12);
+              }
+            } else{
+              posY = dataSet[val];
+            }
+          }
+        });
+
+        target[i].style.transform = 'translate3d(0px, ' + posY + 'px, 0px)';
+      }
+    });
+  }
 
   window.addEventListener('load', function() {
-    viewportSize = viewportOffset = null;
-  });
+    init();
+  })
 
-  window.addEventListener('load', function() {
-
-  });
-}
-
-function onLoad() {
-  addEvent();
-}
-
-window.addEventListener('load', function() {
-  onLoad();
-})
+}(window, document))
